ed62ee8ac07c1154f02dc01e5c8922f2d057f1b6
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-03 18:57:42 +0000

    Compare with solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -16,5 +16,19 @@ const repeatString = function(string, num) {
 
 };
 
+/*
+Solution:
+
+const repeatString = function(word, times) {
+  if (times < 0) return 'ERROR';
+  let string = '';
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
+
+*/
+
 // Do not edit below this line
 module.exports = repeatString;
