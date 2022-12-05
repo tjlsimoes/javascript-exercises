5925eb8d29d351746884c031bd0a948390d6ac84
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-05 19:46:11 +0000

    Complete second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function(arrayOr, tbremov) {
+const removeFromArray = function(arrayOr, ...tbremov) {
+
+    for (i = 0; i < tbremov.length; i++) {
+
+        if (arrayOr.includes(tbremov[i])) {
+
+            let removind = arrayOr.lastIndexOf(tbremov[i]);
+
+            arrayOr.splice(removind, 1);
+    }        
+}
+    return arrayOr;
+}
+
+
+/*
+const removeFromArray = function(arrayOr, ...tbremov) {
 
     function checkFtoL(matchone) {
 
@@ -16,45 +32,6 @@ const removeFromArray = function(arrayOr, tbremov) {
         arrayOr.splice(firstMatch, 1);
 
         return arrayOr;
-
-    } else if (firstMatch !== lastMatch){
-
-        let arrayOrLeng = arrayOr.length;
-
-        for (i = 0; i < arrayOrLeng; i++ ) {
-
-            if(arrayOr[i] == tbremov) {
-
-                arrayOr.splice(i);
-            }
-        }
-
-        return arrayOr;
-
-    } else {
-
-        return arrayOr;
-    }
-};
-
-
-/*
-const removeFromArray = function(arrayOr, tbremov) {
-
-    function checkFtoL(matchone) {
-
-        return matchone === tbremov;
-    
-    }
-    
-
-        
-    let firstMatch = arrayOr.findIndex(checkFtoL);
-    let lastMatch = arrayOr.lastIndexOf(tbremov);
-
-    if (firstMatch === lastMatch) {
-
-        return arrayOr.splice(firstMatch);
 
     } else if (firstMatch !== lastMatch){
 
