bfca5ae0691a0e90f8aa572cb61aac8a647cb797
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-05 18:50:54 +0000

    Complete first test with already fleshed code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,38 @@
-const removeFromArray = function (arrayOr, tbremov) {
+const removeFromArray = function(arrayOr, tbremov) {
 
-    if (arrayOr.includes(tbremov)) {
+    function checkFtoL(matchone) {
 
-        let tbremovind = arrayOr.lastIndexOf(tbremov);
+        return matchone === tbremov;
+    
+    }
+    
 
-        arrayOr.splice(tbremovind, 1);
+        
+    let firstMatch = arrayOr.findIndex(checkFtoL);
+    let lastMatch = arrayOr.lastIndexOf(tbremov);
+
+    if (firstMatch === lastMatch) {
+
+        arrayOr.splice(firstMatch, 1);
+
         return arrayOr;
+
+    } else if (firstMatch !== lastMatch){
+
+        let arrayOrLeng = arrayOr.length;
+
+        for (i = 0; i < arrayOrLeng; i++ ) {
+
+            if(arrayOr[i] == tbremov) {
+
+                arrayOr.splice(i);
+            }
+        }
+
+        return arrayOr;
+
     } else {
+
         return arrayOr;
     }
 };
@@ -49,6 +75,17 @@ const removeFromArray = function(arrayOr, tbremov) {
         return arrayOr;
     }
 };
+
+
+if (arrayOr.includes(tbremov)) {
+
+        let tbremovind = arrayOr.lastIndexOf(tbremov);
+
+        arrayOr.splice(tbremovind, 1);
+        return arrayOr;
+    } else {
+        return arrayOr;
+    }
 */
 // Do not edit below this line
 module.exports = removeFromArray;
