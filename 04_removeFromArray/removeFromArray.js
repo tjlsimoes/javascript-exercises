07a5c1f0dc02fdaf26be62502345e707deb90aec
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-05 17:04:32 +0000

    Layout basic scheme of work
    First test failed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,59 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayOr, tbremov) {
 
+    function checkFtoL(matchone) {
+
+        return matchone === tbremov;
+    
+    }
+    
+    function checkLtoF(matchlast) {
+    
+        return matchlast === tbremov;
+    
+    }
+
+        
+    let firstMatch = arrayOr.findIndex(checkFtoL);
+    let lastMatch = arrayOr.lastIndexOf(checkLtoF);
+
+    if (firstMatch === lastMatch) {
+
+        return arrayOr.splice(arrayOr[firstMatch]);
+
+    } else if (firstMatch !== lastMatch){
+
+        let arrayOrLeng = arrayOr.length;
+
+        for (i = 0; i < arrayOrLeng; i++ ) {
+
+            if(arrayOr[i] == tbremov) {
+
+                arrayOr.splice(i);
+            }
+        }
+
+        return arrayOr;
+
+    } else {
+
+        return arrayOr;
+    }
 };
 
+
+/*
+function checkFtoL(matchone) {
+
+    return matchone === tbremov;
+
+}
+
+function checkLtoF(matchlast) {
+
+    return matchlast === tbremov;
+
+} 
+
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
