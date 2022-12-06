9107529919744f8b7e593506ebcd46b175ca8bda
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-06 12:20:51 +0000

    Complete first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(invalue, fivalue) {
 
+    const range = [];
+
+    for (i= invalue; i <= fivalue ; i++) {
+
+        range.push(i);
+    }
+
+    let sum = 0;
+
+    for (j = 0; j < range.length; j++) {
+
+        sum += range[j];
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
