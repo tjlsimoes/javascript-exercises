a67d517c0e0dd0a138d0c5a0416aec17f9d9dd9b
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-06 12:30:40 +0000

    Complete second test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,19 +2,41 @@ const sumAll = function(invalue, fivalue) {
 
     const range = [];
 
-    for (i= invalue; i <= fivalue ; i++) {
+    if (invalue == NaN || invalue < 0 || fivalue == NaN || fivalue < 0){
+        
+        return "ERROR";
+    } else if (invalue < fivalue) {
 
-        range.push(i);
+        for (i= invalue; i <= fivalue ; i++) {
+
+            range.push(i);
+        }
+    
+        let sum = 0;
+    
+        for (j = 0; j < range.length; j++) {
+    
+            sum += range[j];
+        }
+    
+        return sum;
+    } else if (invalue > fivalue) {
+
+        for (i= fivalue; i <= invalue ; i++) {
+
+            range.push(i);
+        }
+    
+        let sum = 0;
+    
+        for (j = 0; j < range.length; j++) {
+    
+            sum += range[j];
+        }
+    
+        return sum;
     }
 
-    let sum = 0;
-
-    for (j = 0; j < range.length; j++) {
-
-        sum += range[j];
-    }
-
-    return sum;
 };
 
 // Do not edit below this line
