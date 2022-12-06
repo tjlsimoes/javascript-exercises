48b81527f451ce2660ddfaab65fdc93c29ee4c96
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-06 12:42:11 +0000

    Compare with solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -42,5 +42,25 @@ const sumAll = function(invalue, fivalue) {
 
 };
 
+/* 
+Solution:
+
+const sumAll = function(min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+All around more elegant and simpler.
+*/
 // Do not edit below this line
 module.exports = sumAll;
