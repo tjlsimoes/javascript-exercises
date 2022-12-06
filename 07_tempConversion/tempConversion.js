62587f9be178e685eb422cf50e8d3a708f28dfce
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-06 17:01:50 +0000

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrstart) {
+
+  let celsiusend = (fahrstart - 32) * (5 / 9);
+  let celsiusendround = (Math.round(celsiusend * 10)) / 10;
+
+  return celsiusendround;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusstart) {
+
+  let fahrend = (celsiusstart * (9 / 5)) + 32;
+  let fahrendround = (Math.round(fahrend * 10)) / 10;
+
+  return fahrendround;
 };
 
 // Do not edit below this line
